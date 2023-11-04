ea12d8fee389d41ccfb1c29029c05f6183724ad2
Serendipity2-0 <130377556+Serendipity2-0@users.noreply.github.com>
2023-11-04 15:22:00 +0530

    i have finished scraping quickr
    i excel file is which i have extracted and the formatted one .
    the price is converted to integers and builtup area is withouit sqft which i have achieved through the code

--- WebScraping/magicbricks_spider.py
+++ WebScraping/magicbricks_spider.py
@@ -61,64 +61,64 @@
 # df.to_excel("housing.xlsx", index=False)
 # print("housing.xlsx")
 
-import requests
-from bs4 import BeautifulSoup
-import time
-import pandas as pd
+# import requests
+# from bs4 import BeautifulSoup
+# import time
+# import pandas as pd
 
-def convert_price(price_str):
-    try:
-        price_str = price_str.strip().replace(",", "")
-        if "Lakh" in price_str:
-            return int(float(price_str.replace("Lakh", "").strip()) * 1e5)
-        elif "Crore" in price_str:
-            return int(float(price_str.replace("Crore", "").strip()) * 1e7)
-        else:
-            return int(price_str)
-    except ValueError:
-        return None
+# def convert_price(price_str):
+#     try:
+#         price_str = price_str.strip().replace(",", "")
+#         if "Lakh" in price_str:
+#             return int(float(price_str.replace("Lakh", "").strip()) * 1e5)
+#         elif "Crore" in price_str:
+#             return int(float(price_str.replace("Crore", "").strip()) * 1e7)
+#         else:
+#             return int(price_str)
+#     except ValueError:
+#         return None
 
-def scrape_quikr_properties():
-    base_url = "https://www.quikr.com/homes/property/residential-apartments-for-sale-in-bangalore-cid_23?page="
-    page_number = 1
-    all_data = []
-    max_pages = 150
+# def scrape_quikr_properties():
+#     base_url = "https://www.quikr.com/homes/property/residential-apartments-for-sale-in-bangalore-cid_23?page="
+#     page_number = 1
+#     all_data = []
+#     max_pages = 150
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-    }
+#     headers = {
+#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+#     }
 
-    while page_number <= max_pages:
-        print(f"Scraping page {page_number}...")
-        response = requests.get(base_url + str(page_number), headers=headers)
+#     while page_number <= max_pages:
+#         print(f"Scraping page {page_number}...")
+#         response = requests.get(base_url + str(page_number), headers=headers)
         
-        if response.status_code != 200:
-            print(f"Failed to retrieve page {page_number}.")
-            print(f"Status code: {response.status_code}")
-            print(response.text)
-            break
+#         if response.status_code != 200:
+#             print(f"Failed to retrieve page {page_number}.")
+#             print(f"Status code: {response.status_code}")
+#             print(response.text)
+#             break
 
-        soup = BeautifulSoup(response.text, 'html.parser')
+#         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Extract data
-        locations = [loc.get_text().strip().split(" in ")[-1] for loc in soup.select('a.notclickb.projectname')]
-        prices = [convert_price(price.get_text().strip()) for price in soup.select('span') if "Lakh" in price.get_text() or "Crore" in price.get_text()]
-        builtup_areas = [int(area.get_text().split()[0]) for area in soup.select('span') if 'sq.ft.' in area.get_text() and area.get_text().split()[0].replace('.', '', 1).isdigit()]
+#         # Extract data
+#         locations = [loc.get_text().strip().split(" in ")[-1] for loc in soup.select('a.notclickb.projectname')]
+#         prices = [convert_price(price.get_text().strip()) for price in soup.select('span') if "Lakh" in price.get_text() or "Crore" in price.get_text()]
+#         builtup_areas = [int(area.get_text().split()[0]) for area in soup.select('span') if 'sq.ft.' in area.get_text() and area.get_text().split()[0].replace('.', '', 1).isdigit()]
 
-        page_data = list(zip(locations, prices, builtup_areas))
-        all_data.extend(page_data)
+#         page_data = list(zip(locations, prices, builtup_areas))
+#         all_data.extend(page_data)
 
-        page_number += 1
-        time.sleep(2)
+#         page_number += 1
+#         time.sleep(2)
 
-    return all_data
+#     return all_data
 
-data = scrape_quikr_properties()
+# data = scrape_quikr_properties()
 
-# Convert data to DataFrame and save to a new Excel file
-df = pd.DataFrame(data, columns=["Location", "Price", "Built-up Area (sq.ft.)"])
-df.to_excel("qucikrr11.xlsx", index=False)
-print("Data saved to 'qucikrr11.xlsx'")
+# # Convert data to DataFrame and save to a new Excel file
+# df = pd.DataFrame(data, columns=["Location", "Price", "Built-up Area (sq.ft.)"])
+# df.to_excel("scraped_data_new_v3.xlsx", index=False)
+# print("Data saved to '.xlsx'")
 
 # import requests
 # from bs4 import BeautifulSoup
@@ -187,3 +187,65 @@ print("Data saved to 'qucikrr11.xlsx'")
 # df = pd.DataFrame(data, columns=["Location", "Price", "Built-up Area (sq.ft.)"])
 # df.to_excel("scraped_data_new4.xlsx", index=False)
 # print("Data saved to 'scraped_data_new4.xlsx'")
+
+import requests
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+
+def convert_price(price_str):
+    try:
+        price_str = price_str.strip().replace(",", "")
+        if "Lakh" in price_str:
+            return int(float(price_str.replace("Lakh", "").strip()) * 1e5)
+        elif "Crore" in price_str:
+            return int(float(price_str.replace("Crore", "").strip()) * 1e7)
+        else:
+            return int(price_str)
+    except ValueError:
+        return None
+
+def scrape_quikr_properties():
+    base_url = "https://www.quikr.com/homes/property/residential-plots-for-sale-in-bangalore-cid_23?page="
+    page_number = 1
+    all_data = []
+    max_pages = 150
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9'
+    }
+
+    while page_number <= max_pages:
+        print(f"Scraping page {page_number}...")
+        response = requests.get(base_url + str(page_number), headers=headers)
+        
+        if response.status_code != 200:
+            print(f"Failed to retrieve page {page_number}.")
+            print(f"Status code: {response.status_code}")
+            print(response.text)
+            break
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Extract data
+        locations = [loc.get_text().strip().split(" in ")[-1] for loc in soup.select('a.notclickb.projectname')]
+        prices = [convert_price(price.get_text().strip()) for price in soup.select('span') if "Lakh" in price.get_text() or "Crore" in price.get_text()]
+        builtup_areas = [int(area.get_text().split()[0]) for area in soup.select('span') if 'sq.ft.' in area.get_text() and area.get_text().split()[0].replace('.', '', 1).isdigit()]
+
+        page_data = list(zip(locations, prices, builtup_areas))
+        all_data.extend(page_data)
+
+        page_number += 1
+        time.sleep(5)  # Increasing the delay
+
+    return all_data
+
+data = scrape_quikr_properties()
+
+# Convert data to DataFrame and save to a new Excel file
+df = pd.DataFrame(data, columns=["Location", "Price", "Built-up Area (sq.ft.)"])
+df.to_excel("residential_v2.xlsx", index=False)
+print("Data saved to 'residential_v2.xlsx'")
